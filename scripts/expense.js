8e42f1ff81fb4c02518e1e1c4d64b0189aba9d65
Giapdo2005 <giapdo1901@gmail.com>
2024-06-13 13:26:37 -0700

    Interactive add expense button to display it on the screen.

---
+++ scripts/expense.js
@@ -0,0 +1,55 @@
+const expenseLog = [{
+  category: 'groceries',
+  priceCents: 4399,
+  date: 'June 14th 2024'
+}, {
+  category: 'golf',
+  priceCents: 5634,
+  date: 'June 15th 2024'
+}]
+
+
+
+function renderExpense() {
+  let expenseHTML = '';
+
+  expenseLog.forEach((purchase) => {
+    expenseHTML += 
+    `
+      <div>${purchase.category}</div>
+      <div>$${(purchase.priceCents / 100).toFixed(2)}</div>
+      <div>${purchase.date}</div>
+      <div><a href="">Delete</a></div>
+      <div><a href="">Update</a></div>
+    `;
+  })
+  document.querySelector('.js-logs').innerHTML = expenseHTML;
+}
+
+function addExpense(event) {
+  event.preventDefault();
+  const inputCategory = document.querySelector('.js-description')
+  const category = inputCategory.value;
+  const inputPrice = document.querySelector('.js-amount');
+  const price = inputPrice.value;
+  const inputDate = document.querySelector('.js-date');
+  const date = inputDate.value;
+
+  if (category === '' || isNaN(price) || date === '') {
+    alert('Please fill in all fields correctly.');
+    return;
+  }
+
+  expenseLog.push({
+    category: category,
+    priceCents: Math.round(price * 100),
+    date: date
+  })
+
+  renderExpense();
+  document.getElementById('expense-form').reset();
+}
+
+document.querySelector('.js-add-btn').addEventListener('click', addExpense);
+
+renderExpense();
